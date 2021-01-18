2763de806a259a9e769bc7bca1caa8157155f29d
Swapnil Jagtap <developedbysj@gmail.com>
2021-01-18 15:57:35 +0530

    completed level 4 milestone

---
+++ level_4/L4-ActiveRecord/todo.rb
@@ -0,0 +1,59 @@
+require "active_record"
+
+class Todo < ActiveRecord::Base
+  def self.add_task(todo)
+    create!(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
+  end
+
+  def self.overdue
+    all.filter { |todo| todo.overdue? }
+  end
+
+  def self.due_today
+    all.filter { |todo| todo.due_today? }
+  end
+
+  def self.due_later
+    all.filter { |todo| todo.due_later? }
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+    puts "Overdue\n"
+    puts self.overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts self.due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts self.due_later.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete!(id)
+    todo = Todo.find(id)
+    todo.completed = true
+    todo.save
+    todo
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def overdue?
+    due_date < Date.today
+  end
+
+  def due_later?
+    due_date > Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+end
